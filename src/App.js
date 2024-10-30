6d9634845e7acaf63e58062ea8c1700757faa335
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2024-10-30 13:36:16 +0000

    Refractor FormSplitBill component

--- src/App.js
+++ src/App.js
@@ -31,7 +31,9 @@ function Button({ children, onClick }) {
 export default function App() {
   const [showAddFriend, setShowAddFriend] = useState(false);
 
-  const [friends, setFriends] = useState(initialFriends)
+  const [friends, setFriends] = useState(initialFriends);
+
+  const [selectedFriend, setSelectedFriend] = useState(null);
 
   function handleShowAddFriend() {
     setShowAddFriend((show) => !show);
@@ -42,33 +44,51 @@ export default function App() {
     setShowAddFriend(false);
   }
 
+  function handleSelection(friend) {
+      // setSelectedFriend(friend);
+    setSelectedFriend((cur) => (cur?.id === friend.id ? 
+    null : friend));
+    setShowAddFriend(false);
+  }
+
   return ( 
     <div className ="app">
       <div className="sidebar">
-          <FriendsList friends={friends}/>
+          <FriendsList 
+            friends={friends} 
+            selectedFriend={selectedFriend} 
+            onSelection={handleSelection} 
+          />
           {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
           <Button onClick={handleShowAddFriend}>{showAddFriend ? 'Close': 'Add friend'}</Button>
       </div> 
 
-      <FormSplitBill />
+      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
     </div> 
   );
 }
 
-function FriendsList({ friends }) {
+function FriendsList({ friends, onSelection, selectedFriend }) {
 
   return (
       <ul>
             {friends.map((friend) => (
-              <Friend friend={friend} key={friend.id}/>
+              <Friend 
+                friend={friend} 
+                key={friend.id} 
+                selectedFriend={selectedFriend}
+                onSelection={onSelection}
+              />
             ))}
       </ul>
   );
 }
 
-function Friend({ friend }) {
+function Friend({ friend, onSelection, selectedFriend }) {
+  const isSelected = selectedFriend?.id === friend.id;
+
   return (
-    <li>
+    <li className={isSelected ? "selected" : "" }>
           <img src={friend.image} alt={friend.name} />
           <h3>{friend.name}</h3>
 
@@ -90,7 +110,7 @@ function Friend({ friend }) {
                   </p>
           )}
 
-          <Button>Select</Button>
+          <Button onClick={() => onSelection(friend)}>{isSelected ? "Close" : "Select"}</Button>
         </li> 
   );
 }
@@ -136,24 +156,40 @@ function FormAddFriend({ onAddFriend }) {
   </form>
 }
 
-function FormSplitBill() {
+function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : '';
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
   return (
     <form className="form-split-bill">
-        <h2>Split a bill with X</h2>
+        <h2>Split a bill with {selectedFriend.name}</h2>
 
         <label>💰 Bill Value</label>
-        <input type="text" />
+        <input 
+          type="text" 
+          value={bill}
+          onChange={(e) => setBill(Number(e.target.value))}
+        />
 
         <label>🕴 Your Expense</label>
-        <input type="text" />
+        <input 
+          type="text" 
+          value={paidByUser}
+          onChange={(e)=> setPaidByUser(Number(e.target.value) > bill ? paidByUser : Number(e.target.value))}  
+        />
 
-        <label>👩🏻‍🤝‍🧑🏾 X's Expense</label>
-        <input type="text" disabled/>
+        <label>👩🏻‍🤝‍🧑🏾 {selectedFriend.name}'s Expense</label>
+        <input type="text" disabled value={paidByFriend}/>
 
         <label>🤑 Who is paying the bill?</label>
-        <select>
+        <select
+          value={whoIsPaying}
+          onChange={(e) => setWhoIsPaying(e.target.value)}  
+       >
           <option value="user">You</option>
-          <option value="friend">X</option>
+          <option value="friend">{selectedFriend.name}</option>
         </select>
 
         <Button>Split bill</Button>
